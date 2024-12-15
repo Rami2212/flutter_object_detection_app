610dd415e3ae2d42bdb8a7d4096dab9da61b8144
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2024-12-15 09:44:40 +0530

    initial commit

---
+++ lib/MySplashPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MySplashPage extends StatefulWidget {
+  const MySplashPage({super.key});
+
+  @override
+  State<MySplashPage> createState() => _MySplashPageState();
+}
+
+class _MySplashPageState extends State<MySplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
